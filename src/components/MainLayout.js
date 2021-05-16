78238d92c175555b628883736dfc139a0e8005d3
mehran13mome <mehran13mome@gmail.com>
2021-05-17 00:14:02 +0430

    Setup deps

---
+++ src/components/MainLayout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Spin} from 'antd'
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Spin spinning={true} />
+        </div>
+    );
+};
+
+export default MainLayout;
