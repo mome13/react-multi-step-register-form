250f17bacb770dbf10a02db6b18e5b86f713ac80
mehran13mome <mehran13mome@gmail.com>
2021-05-20 23:35:05 +0430

    Add form in right panel

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import {Spin, Card, Col, Row} from 'antd'
+import {Col, Row, Form, Button, Input, Steps} from 'antd'
 import {RightCircleFilled } from '@ant-design/icons'
 import logo from '../logo.svg'
 
+const { Step } = Steps;
 
+const steps = [
+    {
+        title: 'Email',
+        content: 'First-content',
+    },
+    {
+        title: 'Password',
+        content: 'Second-content',
+    },
+    {
+        title: 'Security',
+        content: 'Last-content',
+    },
+];
 
 const MainLayout = () => {
+
+    const [current, setCurrent] = React.useState(0);
+
+    const next = () => {
+        setCurrent(current + 1);
+    };
+
+    const prev = () => {
+        setCurrent(current - 1);
+    };
+
 
     return (
         <div className="MainLayout">
@@ -15,17 +41,16 @@ const MainLayout = () => {
                         boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
                         transition: '0.3s'
                     }}>
-                    <Col  span={9}
-                          style={{
+                    <Col span={9}
+                         style={{
                               height:'100%',
                               background:'#ffd700',
                               borderTopLeftRadius:'30px',
                               borderBottomLeftRadius:'30px',
                               padding:'20px',
-
                           }}>
-                        <div style={{}}>
-                            <img style={{margin:'6rem auto 3rem auto', display: 'block'}} width={150} height={150} alt='happy-girl' src={logo}/>
+                        <div>
+                            <img style={{margin:'6rem auto 3rem auto', display: 'block', position:'relative'}} width={150} height={150}  alt='happy-girl' src={logo}/>
                             <h2 style={{textAlign:'center', margin:'2rem 0 1rem 0'}}>Let's get you set up</h2>
                             <p style={{textAlign:'center'}}>It should only take a couple of minutes to pair with your watch</p>
                             <RightCircleFilled style={{ fontSize: '280%', margin:'2rem auto', display: 'block'}} />
@@ -35,7 +60,45 @@ const MainLayout = () => {
                         borderTopRightRadius:'30px',
                         borderBottomRightRadius:'30px',
                         padding:'20px'}}>
-                        <p>this is right</p>
+                        <Steps style={{margin: '1rem auto', width:'80%'}} progressDot  current={current}>
+                            {steps.map(item => (
+                                <Step key={item.title} title={item.title} />
+                            ))}
+                        </Steps>
+
+                        <Form
+                            layout="horizontal"
+                            // form={form}
+                            name="login"
+                            onFinish={next}
+                            scrollToFirstError
+                            requiredMark={false}
+                        >
+                            <Form.Item
+                                name='email'
+                                label='EMAIL'
+                                rules={[
+                                    {
+                                        type:'email',
+                                        message: 'EMAIL IS NOT VALID',
+                                    },
+                                    {
+                                        required: true,
+                                        message: 'EMAIL IS REQUIRED',
+                                    },
+                                ]}
+                            >
+                                <Input autoComplete="on" autoCorrect="false" autoCapitalize="off" spellCheck="false" />
+                            </Form.Item>
+                            <div style={{display:'flex', flexDirection:'row' , alignItems:'center', alignContent:'center'}}>
+                                <Button style={{}} type="primary" htmlType="submit">
+                                    salam
+                                </Button>
+                                <Button onClick={() => prev()} style={{}} type="secondary" >
+                                    salam
+                                </Button>
+                            </div>
+                        </Form>
                     </Col>
                     </Row>
         </div>
